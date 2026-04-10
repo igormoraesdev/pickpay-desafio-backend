c4314dea7c21e7861cf6d44f2a9ddc5db2abf0fa
Igor Moraes <igormoraesdev@gmail.com>
2026-04-10 16:00:37 -0300

    chore: update user interface param type

--- src/users/user.repository.interface.ts
+++ src/users/user.repository.interface.ts
@@ -1,6 +1,9 @@
+import { CreateUserDto } from "./create-user.dto";
+import { UserDto } from "./user.dto";
+
 export interface IUsersRepository {
   findByEmailOrCpf(email: string, cpfCnpj: string): Promise<any[]>;
-  create(data: any): Promise<any>;
+  create(data: CreateUserDto): Promise<UserDto>;
 }
 
 export const USERS_REPOSITORY = Symbol('USERS_REPOSITORY');
