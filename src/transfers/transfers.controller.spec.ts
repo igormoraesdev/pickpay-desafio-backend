914bbb214af12ebb11e6953d3b0edac65a575403
Igor Moraes <igormoraesdev@gmail.com>
2026-04-12 15:41:05 -0300

    chore: add test controller

--- src/transfers/transfers.controller.spec.ts
+++ src/transfers/transfers.controller.spec.ts
@@ -1,20 +1,41 @@
+import { describe, it, expect, beforeEach, mock } from 'bun:test';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TransfersController } from './transfers.controller';
 import { TransfersService } from './transfers.service';
 
 describe('TransfersController', () => {
   let controller: TransfersController;
+  let service: TransfersService;
+
+  const mockWalletsService = {
+    transfer: mock(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TransfersController],
-      providers: [TransfersService],
+      providers: [
+        {
+          provide: TransfersService,
+          useValue: mockWalletsService,
+        },
+      ],
     }).compile();
 
     controller = module.get<TransfersController>(TransfersController);
+    service = module.get<TransfersService>(TransfersService);
   });
 
-  it('should be defined', () => {
+  it('should create a trasnfer', async () => {
+    const transfer = {
+      payee: 1,
+      payer: 2,
+      value: 100.0,
+    };
+    mockWalletsService.transfer.mockResolvedValue(transfer);
+
+    const result = await controller.create(transfer);
+
     expect(controller).toBeDefined();
   });
 });
